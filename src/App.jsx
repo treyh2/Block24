f5d7f04943354c2fc30d5923b6829f10506c87a1
Your Name <your_email@example.com>
2024-02-19 22:48:30 -0600

    finished code

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
+import { useState } from 'react'
+import { puppyList } from './data.js'
 import './App.css'
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+console.log("puppyList: ", puppyList);
+function handleClick(clickedPupId){
+  const clickedPuppy = puppies.find(puppy => puppy.id === clickedPupId);
+  if (clickedPuppy)
+  setFeatPupId(clickedPupId)
 
-  return (
-    <>
-      <div>
-       
+  console.log(clickedPuppy)
+  const tricksString = clickedPuppy.tricks.map(trick => ` ${trick.title}`).join(', ');
+  alert(`
+      Puppy ID: ${clickedPuppy.id} \n
+      Name: ${clickedPuppy.name} \n
+      Age: ${clickedPuppy.age} \n
+      Email: ${clickedPuppy.email} \n
+      Tricks: ${tricksString}`);
+} return (
+  <div className='App'>
+    <div className='header'>
+      <h1>List of Puppies</h1>
       </div>
-     
-    </>
+      <div className='puppyApp'>
+      {puppies.map((puppy) => {
+        return (<p className= "dog-name" onClick={() => handleClick(puppy.id)} key={puppy.id}>{puppy.name}</p>); 
+      })}
+      </div>
+    </div>
   )
 }
 
